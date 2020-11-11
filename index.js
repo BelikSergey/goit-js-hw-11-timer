156756154f18a6c64c5abf387a76862d3292ed5e
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-11-11 20:37:20 +0200

    Вносит поправки в таймер

--- index.js
+++ index.js
@@ -19,22 +19,23 @@ class CountdownTimer {
     constructor({ targetDate, selector, ontick }) {
         this.selector = document.querySelector(selector)
         this.targetDate = targetDate;
-        this.ontick = ontick;
+        // this.ontick = ontick;
         // this.updateTimer();
         this.start();
+        this.updateTimer();
     }
     start () {
-    setInterval(() => {
-        const currentTime = Date.now();
-        const deltaTime = this.targetDate - currentTime;
-        const time = getTimeComponents(deltaTime);
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = this.targetDate - currentTime;
+            const time = getTimeComponents(deltaTime);
         updateTimer(time);
         // console.log(`${time.days}:${time.hours}:${time.mins}:${time.secs}`);
     }, 1000);
         
             function pad(value) {
                 return String(value).padStart(2,'0');    
-    };
+        };
             function getTimeComponents(time) {
                 const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
                 const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
@@ -42,25 +43,28 @@ class CountdownTimer {
                 const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
                 return { days, hours, mins, secs };     
         };
+            function updateTimer({ days, hours, mins, secs }) {
+                refs.days.textContent = days;
+                refs.hours.textContent = hours;
+                refs.mins.textContent = mins;
+                refs.secs.textContent = secs; 
+            };
     }
 
-    
- 
-
 };
 
-function updateTimer({ days, hours, mins, secs }) {
-    refs.days.textContent = days;
-    refs.hours.textContent = hours;
-    refs.mins.textContent = mins;
-    refs.secs.textContent = secs; 
-};
+// function updateTimer({ days, hours, mins, secs }) {
+//     refs.days.textContent = days;
+//     refs.hours.textContent = hours;
+//     refs.mins.textContent = mins;
+//     refs.secs.textContent = secs; 
+// };
         
 
     const timer = new CountdownTimer ({
         selector: '#timer-1',
         targetDate: new Date('Jan 01, 2021'),
-        ontick: updateTimer,
+        // ontick: updateTimer,
     });
 
 function messageOnClik(event) {
